adce6a6ed193aa7a5492e668421d1ceac381c9a7
Christian <c.herrera14@ufromail.cl>
2021-11-15 18:43:32 -0300

    2

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,4 +1,4 @@
-import {Box, List, ListItemButton} from "@mui/material";
+import {Box, List, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import '../styles/MenuList.scss'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
@@ -9,16 +9,31 @@ function MenuList() {
     const [open, setOpen] = useState(false)
     return (
         <div>
-            <Box sx={{backgroundColor: '#ACDDF3', py:'5px', borderRadius: '9px 9px 0 0'}}>
+            <Box sx={{backgroundColor: '#ACDDF3', py:'5px', borderRadius: '9px 9px 0 0', maxWidth: '532px'}}>
                 {/*<ul>*/}
                 {/*    <li>Home <ExpandLessIcon></ExpandLessIcon> </li>*/}
                 {/*    <li>Base de Datos<ExpandLessIcon></ExpandLessIcon></li>*/}
                 {/*    <li>Búsqueda <ExpandMoreIcon></ExpandMoreIcon> </li>*/}
                 {/*</ul>*/}
                 <List>
-                    <ListItemButton>Home</ListItemButton>
-                    <ListItemButton>Base de datos</ListItemButton>
-                    <ListItemButton>Búsqueda</ListItemButton>
+                    <ListItemButton>
+                        <ListItemText>Home</ListItemText>
+                        <ListItemIcon>
+                            <ExpandMoreIcon></ExpandMoreIcon>
+                        </ListItemIcon>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemText>Base de datos</ListItemText>
+                        <ListItemIcon>
+                            <ExpandMoreIcon></ExpandMoreIcon>
+                        </ListItemIcon>
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemText>Búsqueda</ListItemText>
+                        <ListItemIcon>
+                            <ExpandMoreIcon></ExpandMoreIcon>
+                        </ListItemIcon>
+                    </ListItemButton>
                 </List>
             </Box>
         </div>
