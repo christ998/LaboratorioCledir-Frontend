925b56a19df03db55e7d40358ae08466a8487a86
Christian <c.herrera14@ufromail.cl>
2021-11-15 18:40:08 -0300

    2

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,15 +1,28 @@
+import {Box, List, ListItemButton} from "@mui/material";
+import '../styles/MenuList.scss'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import {useState} from "react";
-import {Box} from "@mui/material";
 
-function menuList() {
 
+function MenuList() {
+    const [open, setOpen] = useState(false)
     return (
         <div>
-            <Box sx={{backgroundColor: '#ACDDF3', width: 300, height: 300}}>
-                dfds
+            <Box sx={{backgroundColor: '#ACDDF3', py:'5px', borderRadius: '9px 9px 0 0'}}>
+                {/*<ul>*/}
+                {/*    <li>Home <ExpandLessIcon></ExpandLessIcon> </li>*/}
+                {/*    <li>Base de Datos<ExpandLessIcon></ExpandLessIcon></li>*/}
+                {/*    <li>Búsqueda <ExpandMoreIcon></ExpandMoreIcon> </li>*/}
+                {/*</ul>*/}
+                <List>
+                    <ListItemButton>Home</ListItemButton>
+                    <ListItemButton>Base de datos</ListItemButton>
+                    <ListItemButton>Búsqueda</ListItemButton>
+                </List>
             </Box>
         </div>
     );
 }
 
-export default menuList
+export default MenuList
