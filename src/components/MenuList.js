1b884ae425ba52fe4fde8b4be465ff766ecb87c8
Christian <c.herrera14@ufromail.cl>
2021-11-15 21:34:51 -0300

    commit

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,4 +1,4 @@
-import {Box, List, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import {Box, createTheme, List, ListItemButton, ListItemIcon, ListItemText, ThemeProvider} from "@mui/material";
 import '../styles/MenuList.scss'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
@@ -7,34 +7,33 @@ import {useState} from "react";
 
 function MenuList() {
     const [open, setOpen] = useState(false)
+    const fontSize = {
+        fontSize: '18px'
+    }
     return (
         <div>
             <Box sx={{backgroundColor: '#ACDDF3', py:'5px', borderRadius: '9px 9px 0 0', maxWidth: '532px'}}>
-                {/*<ul>*/}
-                {/*    <li>Home <ExpandLessIcon></ExpandLessIcon> </li>*/}
-                {/*    <li>Base de Datos<ExpandLessIcon></ExpandLessIcon></li>*/}
-                {/*    <li>Búsqueda <ExpandMoreIcon></ExpandMoreIcon> </li>*/}
-                {/*</ul>*/}
                 <List>
                     <ListItemButton>
-                        <ListItemText classes="font-links">Home</ListItemText>
+                        <ListItemText disableTypography={true} sx={fontSize} >Home</ListItemText>
                         <ListItemIcon>
                             <ExpandMoreIcon></ExpandMoreIcon>
                         </ListItemIcon>
                     </ListItemButton>
                     <ListItemButton>
-                        <ListItemText>Base de datos</ListItemText>
+                        <ListItemText disableTypography={true} sx={fontSize}>Base de datos</ListItemText>
                         <ListItemIcon>
                             <ExpandMoreIcon></ExpandMoreIcon>
                         </ListItemIcon>
                     </ListItemButton>
                     <ListItemButton>
-                        <ListItemText>Búsqueda</ListItemText>
+                        <ListItemText disableTypography={true} sx={fontSize}>Búsqueda</ListItemText>
                         <ListItemIcon>
                             <ExpandMoreIcon></ExpandMoreIcon>
                         </ListItemIcon>
                     </ListItemButton>
                 </List>
+
             </Box>
         </div>
     );
