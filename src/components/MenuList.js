92495c972497c5bfdf643813716bdee805edec48
Christian <c.herrera14@ufromail.cl>
2021-11-15 23:50:55 -0300

    commit

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,4 +1,11 @@
-import {Box, createTheme, List, ListItemButton, ListItemIcon, ListItemText, ThemeProvider} from "@mui/material";
+import {
+    Box,
+    Collapse,
+    List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+} from "@mui/material";
 import '../styles/MenuList.scss'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
@@ -6,33 +13,64 @@ import {useState} from "react";
 
 
 function MenuList() {
-    const [open, setOpen] = useState(false)
+    const [openHome, setOpenHome] = useState(false)
+    const [openBd, setOpenBd] = useState(false)
+    const [openSearcher, setOpenSearcher] = useState(false)
     const fontSize = {
         fontSize: '18px'
     }
+
+    const handleClick = (event) => {
+
+        switch (event.target.id) {
+            case '2':
+                setOpenBd(!openBd);
+                break;
+            case '3':
+                setOpenSearcher(!openSearcher);
+                break;
+        }
+    }
     return (
         <div>
-            <Box sx={{backgroundColor: '#ACDDF3', py:'5px', borderRadius: '9px 9px 0 0', maxWidth: '532px'}}>
+            <Box sx={{backgroundColor: '#ACDDF3', py: '5px', borderRadius: '9px 9px 0 0', maxWidth: '532px'}}>
                 <List>
                     <ListItemButton>
-                        <ListItemText disableTypography={true} sx={fontSize} >Home</ListItemText>
+                        <ListItemText disableTypography={true} sx={fontSize}>Home</ListItemText>
                         <ListItemIcon>
-                            <ExpandMoreIcon></ExpandMoreIcon>
+                            {openHome ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
                         </ListItemIcon>
                     </ListItemButton>
-                    <ListItemButton>
-                        <ListItemText disableTypography={true} sx={fontSize}>Base de datos</ListItemText>
+                    <ListItemButton onClick={handleClick}>
+                        <ListItemText disableTypography={true} sx={fontSize} id={2}>
+                            Base de datos
+                        </ListItemText>
                         <ListItemIcon>
-                            <ExpandMoreIcon></ExpandMoreIcon>
+                            {openBd ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
                         </ListItemIcon>
                     </ListItemButton>
-                    <ListItemButton>
-                        <ListItemText disableTypography={true} sx={fontSize}>Búsqueda</ListItemText>
+                    <ListItemButton onClick={handleClick}>
+                        <ListItemText disableTypography={true} sx={fontSize} id={3}>Búsqueda</ListItemText>
                         <ListItemIcon>
-                            <ExpandMoreIcon></ExpandMoreIcon>
+                            {openSearcher ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
                         </ListItemIcon>
                     </ListItemButton>
+
                 </List>
+                <Collapse in={openBd} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton sx={{pl: 4}}>
+                            <ListItemText primary="Starred"/>
+                        </ListItemButton>
+                    </List>
+                </Collapse>
+                <Collapse in={openSearcher} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton sx={{pl: 4}}>
+                            <ListItemText primary="Búsqueda"/>
+                        </ListItemButton>
+                    </List>
+                </Collapse>
 
             </Box>
         </div>
